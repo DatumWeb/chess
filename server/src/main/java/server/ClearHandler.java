d39e2e26397d5c7f835661a4ea59d5bc2ac63452
DatumCode <ldhymas44@gmail.com>
2025-03-02 16:32:18 -0700

    Added The test files Add function to copy chessboard, start logic of valid moves Add incomplete skelton and code Finish untested logic of is in check, validMoves, makeMove Add untested isInCheckmate function add untested stalemate function
    made some logic tweaks to makeMove
    add helper functions and changed logic to be clearer this fixed bugs
    
    passed last test.
    
    Part 3
    Copy over server and server.java
    
    Copy over dataaccess and file
    
    Create and Copy Test folder with StandardAPITests
    
    Create and Copy the web resources
    
    Maven Dependencies
    
    Server object creation
    
    Add phase 2 img to foles for refrence
    
    Create some files I think I will need. Create the Data Model Classes
    
    Add some random stuff I think I will need for phase 3, clean up lots of code from phase 0 and 1
    
    using this to commit and push to test code sytle

--- server/src/main/java/server/ClearHandler.java
+++ server/src/main/java/server/ClearHandler.java
@@ -6,18 +6,4 @@ import spark.Request;
 import spark.Response;
 import spark.Route;
 
-public class ClearHandler implements Route {
-    @Override
-    public Object handle(Request req, Response res) {
-        try {
-            ClearService clearService = new ClearService();
-            clearService.clearAll(); // Call the service layer to clear data
 
-            res.status(200);
-            return "{}"; // Return empty JSON response for success
-        } catch (Exception e) {
-            res.status(500);
-            return new Gson().toJson(new ErrorResponse("Internal Server Error"));
-        }
-    }
-}
