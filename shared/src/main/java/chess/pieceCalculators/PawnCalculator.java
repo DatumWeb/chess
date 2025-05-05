1fc191aefc6de4857d03b67dd11704231215e407
DatumCode <ldhymas44@gmail.com>
2025-05-04 20:30:48 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic

--- shared/src/main/java/chess/pieceCalculators/PawnCalculator.java
+++ shared/src/main/java/chess/pieceCalculators/PawnCalculator.java
@@ -1,6 +1,7 @@
 package chess.pieceCalculators;
 
 import chess.ChessBoard;
+import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPosition;
 
@@ -10,6 +11,69 @@ import java.util.Set;
 public class PawnCalculator implements pieceMoveCalculator {
     public Set<ChessMove> getMoves(ChessBoard gameBoard, ChessPosition activePosition){
         Set<ChessMove> moves = new HashSet<>();
-        return null;
+
+        ChessGame.TeamColor activeColor = gameBoard.getColorOnSquare(activePosition);
+
+        int stepDirection = 0;
+        if (activeColor == ChessGame.TeamColor.WHITE) {
+            stepDirection = 1;
+        } else {
+            stepDirection = -1; //if black
+        }
+
+        //call the move helper functions
+        getForwardMoves(gameBoard, moves, activePosition, stepDirection, activeColor);
+
+        getAttackMoves (gameBoard, moves, activePosition, stepDirection, activeColor);
+
+        return moves;
+    }
+
+    public void getForwardMoves (ChessBoard gameBoard, Set<ChessMove> moves, ChessPosition activePosition, int stepDirection, ChessGame.TeamColor activeColor) {
+        int testRow = activePosition.getRow() + stepDirection;
+        int testCol = activePosition.getColumn();
+
+        ChessPosition testPositionOneStep = new ChessPosition(testRow, testCol);
+        ChessPosition testPositionTwoStep = new ChessPosition(testRow * 2, testCol);
+
+
+        if (isStartingTurn(activePosition, activeColor)){
+            if (gameBoard.getPiece(testPositionOneStep) == null && isRealPosition(testPositionOneStep)){
+                moves.add(new ChessMove(activePosition, testPositionOneStep, null));
+
+                if(gameBoard.getPiece(testPositionTwoStep) == null && isRealPosition(testPositionOneStep)){
+                    moves.add(new ChessMove(activePosition, testPositionTwoStep, null));
+                }
+            }
+        } else {
+            if (gameBoard.getPiece(testPositionOneStep) == null && isRealPosition(testPositionOneStep)) {
+                moves.add(new ChessMove(activePosition, testPositionOneStep, null));
+            }
+        }
+
+    }
+
+    public void getAttackMoves (ChessBoard gameBoard, Set<ChessMove> moves, ChessPosition activePosition, int stepDirection, ChessGame.TeamColor activeColor) {
+        int testRow = activePosition.getRow() + stepDirection;
+        int testColRight = activePosition.getColumn() + 1;
+        int testColLeft = activePosition.getColumn() - 1;
+
+        ChessPosition testLeft = new ChessPosition(testRow, testColLeft);
+        ChessPosition testRight = new ChessPosition(testRow, testColRight);
+
+        if (gameBoard.getColorOnSquare(testRight) != activeColor && isRealPosition(testRight) && gameBoard.getPiece(testRight) != null){
+            moves.add(new ChessMove(activePosition, testRight, null));
+        }
+        if (gameBoard.getColorOnSquare(testLeft) != activeColor && isRealPosition(testLeft) && gameBoard.getPiece(testLeft) != null) {
+            moves.add(new ChessMove(activePosition, testLeft, null));
+        }
+    }
+
+    public boolean isPromotionRow (ChessPosition testPosition) {
+        return testPosition.getRow() == 1 || testPosition.getRow() == 8;
+    }
+
+    public boolean isStartingTurn (ChessPosition activePosition, ChessGame.TeamColor activeColor) {
+        return (activePosition.getColumn() == 2 && activeColor == ChessGame.TeamColor.WHITE) || (activePosition.getColumn() == 7 && activeColor == ChessGame.TeamColor.BLACK);
     }
 }
--- shared/src/main/java/chess/pieceCalculators/QueenCalculator.java
+++ shared/src/main/java/chess/pieceCalculators/QueenCalculator.java
@@ -10,6 +10,37 @@ import java.util.Set;
 public class QueenCalculator implements pieceMoveCalculator {
     public Set<ChessMove> getMoves(ChessBoard gameBoard, ChessPosition activePosition){
         Set<ChessMove> moves = new HashSet<>();
-        return null;
+
+        int [][] potentialMoves = {{0, 1}, {1, 0}, {1, 1}, {0, -1}, {-1, 0}, {1, -1}, {-1, 1}, {-1, -1}};
+
+
+        for (int [] jump: potentialMoves){
+            int step = 1;
+
+            while(true) {
+
+                int testRow = activePosition.getRow() + jump[1] * step;
+                int testCol = activePosition.getColumn() + jump[0] * step;
+
+                ChessPosition testPosition = new ChessPosition(testRow, testCol);
+
+                if (!isRealPosition(testPosition)){
+                    break;
+                }
+                if (gameBoard.getPiece(testPosition) == null) {
+                    moves.add(new ChessMove(activePosition, testPosition, null));
+                    step++;
+                } else if (gameBoard.getColorOnSquare(testPosition) != gameBoard.getColorOnSquare(activePosition)) {
+                    moves.add(new ChessMove(activePosition, testPosition, null));
+                    break;
+                } else {
+                    break;
+                }
+            }
+
+        }
+
+        return moves;
+
     }
 }
