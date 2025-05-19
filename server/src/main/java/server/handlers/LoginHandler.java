3391a5b25052078e4f885b96a31f04253168bdaa
DatumCode <ldhymas44@gmail.com>
2025-05-19 16:49:49 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService
    
    add JoinGameHandler and service
    
    create LoginHandler and service

--- server/src/main/java/server/handlers/LoginHandler.java
+++ server/src/main/java/server/handlers/LoginHandler.java
@@ -1,4 +1,47 @@
 package server.handlers;
 
-public class LoginHandler {
+import com.google.gson.Gson;
+import service.LoginService;
+import model.AuthData;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import dataaccess.DataAccessException;
+
+import java.util.Map;
+
+public class LoginHandler implements Route {
+    private final Gson gson = new Gson();
+    private final LoginService loginService;
+
+    public LoginHandler(LoginService loginService) {
+        this.loginService = loginService;
+    }
+
+    @Override
+    public Object handle(Request req, Response res) {
+        try {
+            Map<String, String> requestBody = gson.fromJson(req.body(), Map.class);
+            String username = requestBody.get("username");
+            String password = requestBody.get("password");
+
+            if (username == null || password == null) {
+                res.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            AuthData authData = loginService.login(username, password);
+
+            res.status(200);
+            return gson.toJson(Map.of("username", authData.username(), "authToken", authData.authToken()));
+
+        } catch (DataAccessException exception) {
+            res.status(401);
+            return gson.toJson(Map.of("message", "Error: unauthorized"));
+
+        } catch (Exception exception) {
+            res.status(500);
+            return gson.toJson(Map.of("message", "Error: Internal Server Error"));
+        }
+    }
 }
--- server/src/main/java/service/LoginService.java
+++ server/src/main/java/service/LoginService.java
@@ -1,4 +1,40 @@
 package service;
 
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import dataaccess.AuthDAO;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
 public class LoginService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDao;
+
+    public LoginService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDao = authDAO;
+    }
+
+    public AuthData login(String username, String password) throws DataAccessException {
+        UserData user = userDAO.getUser(username);
+        if (user == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        if (!user.password().equals(password)) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        AuthData authData = new AuthData(generateToken(), username);
+        authDao.createAuthToken(authData);
+        return authData;
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+
 }
