f29f948cd127228c23f8ec83151aafcf9391518e
DatumCode <ldhymas44@gmail.com>
2025-05-19 17:48:13 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService
    
    add JoinGameHandler and service
    
    create LoginHandler and service
    
    create LogoutHandler and service
    
    create RegisterHandler and service
    RegisterHandler and service

--- server/src/main/java/server/handlers/RegisterHandler.java
+++ server/src/main/java/server/handlers/RegisterHandler.java
@@ -1,4 +1,47 @@
 package server.handlers;
 
-public class RegisterHandler {
+import com.google.gson.Gson;
+import service.RegisterService;
+import model.AuthData;
+import dataaccess.DataAccessException;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Map;
+
+public class RegisterHandler implements Route {
+    private final RegisterService registerService;
+    private final Gson gson = new Gson();
+
+    public RegisterHandler(RegisterService registerService) {
+        this.registerService = registerService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            Map<String, String> body = gson.fromJson(request.body(), Map.class);
+            String username = body.get("username");
+            String password = body.get("password");
+            String email = body.get("email");
+
+            AuthData authData = registerService.register(username, password, email);
+
+            response.status(200);
+            return gson.toJson(authData);
+
+        } catch (DataAccessException exception) {
+
+            if (exception.getMessage().equals("Error: already taken")) {
+                response.status(403);
+                return gson.toJson(Map.of("message", "Error: already taken"));
+            }
+            response.status(400);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        } catch (Exception exception) {
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        }
+    }
 }
--- server/src/main/java/service/RegisterService.java
+++ server/src/main/java/service/RegisterService.java
@@ -1,4 +1,46 @@
 package service;
 
+import dataaccess.*;
+import model.AuthData;
+import model.UserData;
+
+import java.util.UUID;
+
 public class RegisterService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public RegisterService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public AuthData register(String username, String password, String email) throws DataAccessException {
+        if (username == null) {
+            throw new DataAccessException("Username is null");
+        }
+        if (password == null) {
+            throw new DataAccessException("Password cannot be null.");
+        }
+        if (email == null) {
+            throw new DataAccessException("Email cannot be null.");
+        }
+
+        if (userDAO.getUser(username) != null) {
+            throw new DataAccessException("Error: already taken");
+        }
+
+        UserData newUser = new UserData(username, password, email);
+        userDAO.createUser(newUser);
+
+        String authToken = generateToken();
+        AuthData authData = new AuthData(authToken, username);
+        authDAO.createAuthToken(authData);
+
+        return authData;
+    }
+
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
 }
