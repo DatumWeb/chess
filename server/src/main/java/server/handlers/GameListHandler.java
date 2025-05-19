e307f373391dc1b52405b1b3b24b0f3c596ed1eb
DatumCode <ldhymas44@gmail.com>
2025-05-19 15:05:00 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService

--- server/src/main/java/server/handlers/GameListHandler.java
+++ server/src/main/java/server/handlers/GameListHandler.java
@@ -1,4 +1,50 @@
 package server.handlers;
 
-public class GameListHandler {
+import com.google.gson.Gson;
+import service.GameListService;
+import dataaccess.DataAccessException;
+import model.GameData;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class GameListHandler implements Route {
+    private final GameListService gameListService;
+    private final Gson gson = new Gson();
+    private static final Logger logger = Logger.getLogger(GameListHandler.class.getName());
+
+    public GameListHandler(GameListService gameListService) {
+        this.gameListService = gameListService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+            logger.info("Auth Token: " + authToken);
+
+            List<GameData> games = gameListService.getGameList(authToken);
+
+            response.status(200);
+            return gson.toJson(Map.of("games", games));
+
+        } catch (DataAccessException exception) {
+            logger.severe("Data Access Error: " + exception.getMessage());
+            if (exception.getMessage().equals("Error: unauthorized")) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        } catch (Exception exception) {
+            logger.severe("General Error: " + exception.getMessage());
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        }
+    }
 }
--- server/src/main/java/service/GameListService.java
+++ server/src/main/java/service/GameListService.java
@@ -1,4 +1,28 @@
 package service;
 
+import dataaccess.GameDAO;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.AuthData;
+
+import java.util.List;
+
 public class GameListService {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameListService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public List<GameData> getGameList(String authToken) throws DataAccessException {
+        AuthData authData = authDAO.getAuthToken(authToken);
+        if (authData == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        return gameDAO.getAllGames();
+    }
 }
