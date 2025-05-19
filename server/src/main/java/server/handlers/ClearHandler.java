a2b66db3694f9a6370f9b11736a64c8442638595
DatumCode <ldhymas44@gmail.com>
2025-05-19 12:00:37 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler

--- server/src/main/java/server/handlers/ClearHandler.java
+++ server/src/main/java/server/handlers/ClearHandler.java
@@ -1,4 +1,38 @@
 package server.handlers;
 
-public class ClearHandler {
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.ClearService;
+import spark.request;
+import spark.Response;
+import spark.Route;
+
+public class ClearHandler implements Route {
+    private final ClearService clearService;
+    private final Gson gson = new Gson();
+
+    public ClearHandler(ClearService clearService) {
+        this.clearService = clearService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            clearService.clear();
+            response.status(200);
+            return "{}";
+        } catch (DataAccessException exception) {
+            response.status(500);
+            return "{\"message\": \"Error: " + exception.getMessage() + "\"}";
+        }
+
+    }
+
+    private static class ClearResponse {
+        private final String message;
+
+        public ClearResponse(String message) {
+            this.message = message;
+        }
+    }
 }
