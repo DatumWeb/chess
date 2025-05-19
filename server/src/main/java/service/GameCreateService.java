6724c6318fabae9d0b34c64ecdb34aa284cb534c
DatumCode <ldhymas44@gmail.com>
2025-05-19 13:52:21 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService

--- server/src/main/java/service/GameCreateService.java
+++ server/src/main/java/service/GameCreateService.java
@@ -1,4 +1,49 @@
 package service;
 
+import chess.ChessBoard;
+import dataaccess.GameDAO;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.AuthData;
+import chess.ChessGame;
+
+import java.util.UUID;
+
 public class GameCreateService {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public GameCreateService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public GameData createGame(String authToken, String gameName) throws DataAccessException {
+        AuthData authData = authDAO.getAuthToken(authToken);
+        if (authData == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        if (gameName == null || gameName.isEmpty()) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        String whiteUsername = null;
+        String blackUsername = null;
+
+        ChessGame game = new ChessGame();
+        ChessBoard gameBoard = new ChessBoard();
+        gameBoard.resetBoard();
+
+        GameData newGame = new GameData(generateGameId(), whiteUsername, blackUsername, gameName, game);
+
+        gameDAO.createGame(newGame);
+
+        return newGame;
+    }
+
+    private int generateGameId() {
+        return Math.abs(UUID.randomUUID().hashCode());
+    }
 }
