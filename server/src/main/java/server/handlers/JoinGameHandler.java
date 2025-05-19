776c45b9c97977989e0d5544fa43525dca9200f6
DatumCode <ldhymas44@gmail.com>
2025-05-19 16:34:47 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService
    
    add JoinGameHandler and service

--- server/src/main/java/server/handlers/JoinGameHandler.java
+++ server/src/main/java/server/handlers/JoinGameHandler.java
@@ -1,4 +1,61 @@
 package server.handlers;
 
-public class JoinGameHandler {
+import com.google.gson.Gson;
+import service.JoinGameService;
+import dataaccess.DataAccessException;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Map;
+
+public class JoinGameHandler implements Route {
+    private final JoinGameService joinGameService;
+    private final Gson gson = new Gson();
+
+    public JoinGameHandler(JoinGameService joinGameService) {
+        this.joinGameService = joinGameService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+            Map<String, Object> body = gson.fromJson(request.body(), Map.class);
+            String playerColor = (String) body.get("playerColor");
+
+            Number gameIDNum = (Number) body.get("gameID");
+            if (gameIDNum == null) {
+                response.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+            }
+
+            int gameID = gameIDNum.intValue();
+
+            joinGameService.joinGame(authToken, gameID, playerColor);
+
+            response.status(200);
+            return gson.toJson(Map.of());
+
+        } catch (DataAccessException exception) {
+            if (exception.getMessage().equals("Error: unauthorized")) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+
+            } else if (exception.getMessage().equals("Error: bad request")) {
+                response.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+
+            } else if (exception.getMessage().equals("Error: already taken")) {
+                response.status(403);
+                return gson.toJson(Map.of("message", "Error: already taken"));
+            }
+
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        } catch (Exception exception) {
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        }
+    }
 }
--- server/src/main/java/service/JoinGameService.java
+++ server/src/main/java/service/JoinGameService.java
@@ -1,4 +1,52 @@
 package service;
 
+import dataaccess.GameDAO;
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.AuthData;
+
 public class JoinGameService {
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public JoinGameService(GameDAO gameDAO, AuthDAO authDAO) {
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public void joinGame(String authToken, int gameID, String playerColor) throws DataAccessException {
+        if (gameID <= 0) {
+            throw new DataAccessException("Error: bad request");
+        }
+        AuthData authData = authDAO.getAuthToken(authToken);
+        if (authData == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        String username = authData.username();
+
+        if (playerColor == null || (!playerColor.equals("WHITE") && !playerColor.equals("BLACK"))) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        GameData gameData = gameDAO.getGame(gameID);
+        if (gameData == null) {
+            throw new DataAccessException("Error: bad request");
+        }
+
+        if (playerColor.equals("WHITE") && gameData.whiteUsername() != null) {
+            throw new DataAccessException("Error: already taken");
+        } else if (playerColor.equals("BLACK") && gameData.blackUsername() != null) {
+            throw new DataAccessException("Error: already taken");
+        }
+
+        if (playerColor.equals("WHITE")) {
+            gameData = new GameData(gameData.gameID(), username, gameData.blackUsername(), gameData.gameName(), gameData.game());
+        } else if (playerColor.equals("BLACK")) {
+            gameData = new GameData(gameData.gameID(), gameData.whiteUsername(), username, gameData.gameName(), gameData.game());
+        }
+
+        gameDAO.updateGame(gameData);
+    }
 }
