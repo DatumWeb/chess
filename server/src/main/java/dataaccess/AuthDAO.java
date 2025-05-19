c73277f6869d73ac53d8c50d916ba55cba806331
DatumCode <ldhymas44@gmail.com>
2025-05-19 09:56:12 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    add more file structure and starting imports
    
    add authDAO interface and models

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,13 @@
 package dataaccess;
 
-public class AuthDAO {
+import model.AuthData;
+
+public interface AuthDAO {
+    AuthData getAuthToken(String authToken) throws DataAccessException;
+
+    void createAuthToken(AuthData authData) throws DataAccessException;
+
+    void deleteAuthToken(String authToken) throws DataAccessException;
+
+    void clearAuth();
 }
