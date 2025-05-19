698f2a300457c29ab0775747ff9258faacd20727
DatumCode <ldhymas44@gmail.com>
2025-05-19 12:22:49 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,5 +1,56 @@
 package server;
 
-public class Server {
 
+import server.handlers.*;
+import service.*;
+import spark.*;
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryGameDAO;
+import dataaccess.MemoryUserDAO;
+
+public class Server {
+    private ClearService clearService;
+    private RegisterService registerService;
+    private LoginService loginService;
+    private LogoutService logoutService;
+    private GameCreateService gameCreateService;
+    private JoinGameService joinGameService;
+    private GameListService gameListService;
+
+
+    public int run(int desiredPort) {
+        Spark.port(desiredPort);
+
+        Spark.staticFiles.location("web");
+
+        // Register your endpoints and handle exceptions here.
+        var userDAO = new MemoryUserDAO();
+        var gameDAO = new MemoryGameDAO();
+        var authDAO = new MemoryAuthDAO();
+
+        clearService = new ClearService(userDAO, gameDAO, authDAO);
+        registerService = new RegisterService(userDAO, authDAO);
+        loginService = new LoginService(userDAO, authDAO);
+        logoutService = new LogoutService(authDAO);
+        gameCreateService = new GameCreateService(gameDAO, authDAO);
+        joinGameService = new JoinGameService(gameDAO, authDAO);
+        gameListService = new GameListService(gameDAO, authDAO);
+
+
+        Spark.delete("/db", new ClearHandler(clearService));
+        Spark.delete("/session", new LogoutHandler(logoutService));
+        Spark.post("/user", new RegisterHandler(registerService));
+        Spark.post("/session", new LoginHandler(loginService));
+        Spark.post("/game", new GameCreateHandler(gameCreateService));
+        Spark.put("/game", new JoinGameHandler(joinGameService));
+        Spark.get("/game", new GameListHandler(gameListService));
+
+        Spark.awaitInitialization();
+        return Spark.port();
+    }
+
+    public void stop() {
+        Spark.stop();
+        Spark.awaitStop();
+    }
 }
