6ad31991d4d4747bc63ec9c37eaf5c7b222f7f06
DatumCode <ldhymas44@gmail.com>
2025-05-19 16:51:07 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService
    
    add JoinGameHandler and service
    
    create LoginHandler and service
    
    create LogoutHandler and service

--- server/src/main/java/server/handlers/LogoutHandler.java
+++ server/src/main/java/server/handlers/LogoutHandler.java
@@ -1,4 +1,48 @@
 package server.handlers;
 
-public class LogoutHandler {
+import com.google.gson.Gson;
+import service.LogoutService;
+import dataaccess.DataAccessException;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Map;
+
+public class LogoutHandler implements Route {
+    private final LogoutService logoutService;
+    private final Gson gson = new Gson();
+
+    public LogoutHandler(LogoutService logoutService) {
+        this.logoutService = logoutService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+
+            if (authToken == null) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            boolean success = logoutService.logout(authToken);
+
+            if (!success) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            }
+
+            response.status(200);
+            return "{}";
+
+        } catch (DataAccessException exception) {
+            response.status(401);
+            return gson.toJson(Map.of("message", exception.getMessage()));
+        } catch (Exception exception) {
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        }
+    }
 }
--- server/src/main/java/service/LogoutService.java
+++ server/src/main/java/service/LogoutService.java
@@ -1,4 +1,26 @@
 package service;
 
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+
 public class LogoutService {
+    private final AuthDAO authDAO;
+
+    public LogoutService(AuthDAO authDAO) {
+        this.authDAO = authDAO;
+    }
+
+    public boolean logout(String authToken) throws DataAccessException {
+        if (authToken == null) {
+            throw new DataAccessException("Auth token is missing.");
+        }
+
+        try {
+            authDAO.deleteAuthToken(authToken);
+        } catch (DataAccessException exception) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        return true;
+    }
 }
