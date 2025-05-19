0e6211bc04712b51805552da16870bbbd0985296
DatumCode <ldhymas44@gmail.com>
2025-05-19 12:09:02 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server

--- server/src/main/java/server/handlers/GameCreateHandler.java
+++ server/src/main/java/server/handlers/GameCreateHandler.java
@@ -1,4 +1,49 @@
 package server.handlers;
 
-public class GameCreateHandler {
+import com.google.gson.Gson;
+import service.GameCreateService;
+import dataaccess.DataAccessException;
+import model.GameData;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+import java.util.Map;
+
+public class GameCreateHandler implements Route {
+    private final GameCreateService gameCreateService;
+    private final Gson gson = new Gson();
+
+    public GameCreateHandler(GameCreateService gameCreateService) {
+        this.gameCreateService = gameCreateService;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) {
+        try {
+            String authToken = request.headers("authorization");
+            Map<String, String> body = gson.fromJson(request.body(), Map.class);
+            String gameName = body.get("gameName");
+
+            GameData gameData = gameCreateService.createGame(authToken, gameName);
+
+            response.status(200);
+            return gson.toJson(Map.of("gameID", gameData.gameID()));
+
+        } catch (DataAccessException exception) {
+            if (exception.getMessage().equals("Error: unauthorized")) {
+                response.status(401);
+                return gson.toJson(Map.of("message", "Error: unauthorized"));
+            } else if (exception.getMessage().equals("Error: bad request")) {
+                response.status(400);
+                return gson.toJson(Map.of("message", "Error: bad request"));
+            }
+
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        } catch (Exception exception) {
+            response.status(500);
+            return gson.toJson(Map.of("message", "Error: " + exception.getMessage()));
+        }
+    }
 }
