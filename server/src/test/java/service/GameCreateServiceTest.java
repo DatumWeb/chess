e4efbc29163b3ace29317338f1c653b32855da7c
DatumCode <ldhymas44@gmail.com>
2025-05-19 19:33:53 -0600

    Impliment few basic functions and declare some class variables
    Add more basic function imlimentations and class variables, Create pieceMoveCalculator
    
    Impliment King Calculator
    
    files for other calculators, and some logic
    
    simple logic/filler for pieceCalculators, calls to those. Helper function to get team color on a spesific square.
    finish some bishop logic
    add hashcode stuff
    
    Add Rook Logic to pass tests, add some knight logic
    
    Fix knight logic
    
    Add King logic
    
    Add Queen Logic
    
    Add some pawn logic
    
    finish pawn logic
    
    finish resetBoard and HashLogic
    commiting to push
    
    Add test files
    
    Add few helper functions
    
    implement makeMove and validMoves
    
    add some helper functions and is in check
    
    Decided didn't want to do extra credit
    Fix namings for Code Style
    Add SteppingMoveCalculator to elminate shared code
    
    Refractor BishopCalculator to use SteppingMoveCalculator
    Refractor King and Knight to use jumpingCalc
    
    add file structure
    
    impliment Memory DAOS
    
    impliment clear handler
    
    impliment GameCreateHandler and main javea to run server
    
    imliment server.java
    Add tests
    
    I don't think you can really see this but I fixed the spark import and dependencies
    
    add ClearService and GameCreateService
    
    add GameListHandler, and GameListService
    
    add JoinGameHandler and service
    
    create LoginHandler and service
    
    create LogoutHandler and service
    
    create RegisterHandler and service
    RegisterHandler and service
    
    fixed a error code
    refractored file structure to pass all tests
    test for code
    
    ClearServiceTest implimented and add testfiles for other services
    
    Add GameCreateServiceTest

--- server/src/test/java/service/GameCreateServiceTest.java
+++ server/src/test/java/service/GameCreateServiceTest.java
@@ -1,4 +1,48 @@
 package service;
 
-public class GameCreateServiceTest {
+import dataaccess.*;
+import model.AuthData;
+import model.GameData;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameCreateServiceTest {
+    private GameCreateService gameCreateService;
+    private GameDAO gameDAO;
+    private AuthDAO authDAO;
+    private String validAuthToken;
+    private String invalidAuthToken;
+    private String gameName = "TestGame";
+
+    @BeforeEach
+    void setUp() throws DataAccessException {
+        gameDAO = new MemoryGameDAO();
+        authDAO = new MemoryAuthDAO();
+        gameCreateService = new GameCreateService(gameDAO, authDAO);
+
+        AuthData authData = new AuthData("validToken", "testUser");
+        authDAO.createAuthToken(authData);
+        validAuthToken = authData.authToken();
+        invalidAuthToken = "invalidToken";
+    }
+
+    @Test
+    @DisplayName("Successfully create a game")
+    void testCreateGameSuccess() throws DataAccessException {
+        GameData createdGame = gameCreateService.createGame(validAuthToken, gameName);
+
+        assertNotNull(createdGame, "Created game should not be null.");
+        assertEquals(gameName, createdGame.gameName(), "Game name should match.");
+    }
+
+    @Test
+    @DisplayName("Fail to create game with invalid auth token")
+    void testCreateGameUnauthorized() {
+        DataAccessException exception = assertThrows(DataAccessException.class, () ->
+                gameCreateService.createGame(invalidAuthToken, gameName)
+        );
+
+        assertEquals("Error: unauthorized", exception.getMessage(), "Error message should indicate unauthorized access.");
+    }
 }
